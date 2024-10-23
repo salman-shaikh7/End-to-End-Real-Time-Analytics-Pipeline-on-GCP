9f47ac52633717ae7b1acc2337a047fcad428925
Salman Jahangir Shaikh <salmanrit1706114@gmail.com>
2024-10-23 19:16:05 -0400

    Created dataset and pub sub topic using dag bash operator

---
+++ bigquery_files/create_dataset.sh
@@ -0,0 +1,2 @@
+#execuate from env/airflow which have necessary IAM Access.
+bq mk --location=US customer_dataset
--- dag_files/dag_python_file.py
+++ dag_files/dag_python_file.py
@@ -8,24 +8,34 @@ def test_py_funtion():
     print("In python testing function")
 
 with DAG(
-    "Testing_Dag_id",
+    "Testing_Dag_for_bucket_dataset_id",
     schedule=None,
     start_date=datetime(2021, 1, 1),
     catchup=False
 ) as dag:
         
     TASK_1= BashOperator(
-                        task_id='task_1_id',
-                        bash_command="""
-                        echo "Running Task 1"
-                        PROJECT_ID=$(gcloud config get-value project)
-                        echo $PROJECT_ID
+                        task_id='Create_PubSub_Topic',
+                        bash_command=
+                        """
+                        echo "Creating pub sub topic"
+                        gcloud pubsub topics create my_topic
+                        echo "pub sub topic created"
                         """
                         )
     
-    TASK_2= PythonOperator(
-                        task_id='task_2_id',
-                        python_callable=test_py_funtion
+    TASK_2= BashOperator(
+                        task_id='Create_BigQuery_Dataset',
+                        bash_command=
+                        """
+                        echo "Creating BigQuery Dataset"
+                        bq mk --location=US customer_dataset
+                        echo "Created BigQuery Dataset"
+                        """
                         )
+    
+    
+    
+
 
 TASK_1>>TASK_2
