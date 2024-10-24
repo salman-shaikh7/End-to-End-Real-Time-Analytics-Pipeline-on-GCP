566a14e89170b063790a3499bdf0db4bc1ea0ebe
Salman Jahangir Shaikh <salmanrit1706114@gmail.com>
2024-10-23 21:53:08 -0400

    dag file updated to execute entire workflow

--- dag_files/dag_python_file.py
+++ dag_files/dag_python_file.py
@@ -1,14 +1,44 @@
+#import required package to build dag
 from datetime import datetime
 from airflow.models.dag import DAG
 
+#import required operators 
 from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
+from airflow.providers.google.cloud.operators.dataflow import DataflowStopJobOperator
+
+#import rquired modules to publish messsage
+import requests
+import json
+from google.cloud import pubsub_v1
+import time 
+
+#Function to fetch and publish message to pub/sub topic
+def fetch_and_publish_message():
+    duration = 5 * 60  
+    start_time = time.time()
+
+    while (time.time() - start_time) < duration:
+        publisher = pubsub_v1.PublisherClient()
+        project_id = "myprojectid7028"
+        topic_id = "my_topic"
+        topic_path = publisher.topic_path(project_id, topic_id)
+
+        data_j=requests.get("https://randomuser.me/api/").json()
+
+        data = data_j['results']
+        message_json = json.dumps(data) # Dict to str
+        message_bytes = message_json.encode("utf-8") # str to bytes (pub/sub acceptable income)
+
+        future = publisher.publish(topic_path, data=message_bytes)
+        print(f"Published message ID: {future.result()}")
+        time.sleep(0.5) # wait for half a second to make 2nd api call
 
 def test_py_funtion():
     print("In python testing function")
 
 with DAG(
-    "Testing_Dag_for_bucket_dataset_id",
+    "pubsub_dataflow_bigquery_workflow_dag",
     schedule=None,
     start_date=datetime(2021, 1, 1),
     catchup=False
@@ -34,8 +64,61 @@ with DAG(
                         """
                         )
     
-    
+    TASK_3=PythonOperator(
+                        task_id='Publish_message_to_pub_sub',
+                        python_callable=fetch_and_publish_message
+                        )
     
 
+    TASK_4=BashOperator(
+                        task_id='wait_for_pipeline_execution',
+                        bash_command="""
+                        echo "waiting for 500 seconds"
+                        sleep 500
+                        echo "wait finished for 500 seconds"
+                        """
+                        )
+    
 
-TASK_1>>TASK_2
+    TASK_5=BashOperator(
+                        task_id='deploy_the_pipeline',
+                        bash_command="""
+                        export PROJECT_ID=$(gcloud config get-value project)
+                        export REGION='us-east1'
+                        export BUCKET=gs://${PROJECT_ID}
+                        export PIPELINE_FOLDER=${BUCKET}
+                        export RUNNER=DataflowRunner
+                        export PUBSUB_TOPIC=projects/${PROJECT_ID}/topics/my_topic
+                        export WINDOW_DURATION=60
+                        export AGGREGATE_TABLE_NAME=${PROJECT_ID}:customer_dataset.customer_insights_by_min 
+
+                        gsutil cp gs://myprojectid7028/scripts/dataflow_job_script.py /tmp/dataflow_job_script.py 
+
+                        python3 /tmp/dataflow_job_script.py \
+                            --project=${PROJECT_ID} \
+                            --region=${REGION} \
+                            --staging_location=${PIPELINE_FOLDER}/staging \
+                            --temp_location=${PIPELINE_FOLDER}/temp \
+                            --runner=${RUNNER} \
+                            --input_topic=${PUBSUB_TOPIC} \
+                            --window_duration=${WINDOW_DURATION} \
+                            --agg_table_name=${AGGREGATE_TABLE_NAME} &
+
+                        echo "Pipeline Deployment Done"     
+                        """
+                        )   
+
+    
+    TASK_6=DataflowStopJobOperator(
+                        task_id="stop_dataflow_job",
+                        location="us-east1",
+                        drain_pipeline=True,
+                        job_name_prefix="streaming-minute-traffic-pipeline-", # this name is assigned during execution of dataflow pipeline
+                    )
+    
+#Map Dependencies    
+
+TASK_1>>TASK_2>>TASK_3
+TASK_2>>TASK_4
+TASK_2>>TASK_5
+TASK_4>>TASK_6
